062ccf1c9c6455c25afa391a221c758d7ce9f6c9
C and K Software <candksoftware@gmail.com>
2019-09-16 00:33:07 +0530

    Updates

---
+++ precache-manifest.1c3fd7fbeb7c38549039a671f862bcb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e60274304169cc69c0c43fe8666344",
+    "url": "/socialme/index.html"
+  },
+  {
+    "revision": "b1cb93f115df92533b8e",
+    "url": "/socialme/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb4416a4b84ee19d60e1",
+    "url": "/socialme/static/js/2.c39984c4.chunk.js"
+  },
+  {
+    "revision": "b1cb93f115df92533b8e",
+    "url": "/socialme/static/js/main.5edf0c29.chunk.js"
+  },
+  {
+    "revision": "40d5cd601cfa8de8b151",
+    "url": "/socialme/static/js/runtime~main.fa1916b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socialme/static/media/logo.5d5d9eef.svg"
+  }
+]);
